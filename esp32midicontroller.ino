a1b1a46471760c897c26c70174fd338e45de065d
tonsofpcs <34757554+tonsofpcs@users.noreply.github.com>
2022-10-14 14:08:06 -0400

    TODO: add useful commit description

--- esp32midicontroller.ino
+++ esp32midicontroller.ino
@@ -1,66 +1,65 @@
 //TODO: add includes, remove default comments
 
 int tbar, fader[4], enc[4], shuttle, jog, btn[9];
-int tbar_o, fader_o[4], enc_o[4], shuttle_o, jog_o, btn_o[9];
-int tbar_m = 1, fader_m[4] = {2,3,4,5}, enc_m[4] = {6,7,8,9}, shuttle_m = 10, jog_m = 11, btn_m[9] = {12,13,14,15,16,17,18,19,20};
-int tbar_p = 1, fader_p[4] = {2,3,4,5}, enc_p[4][2] = {{6,7},{8,9},{10,11},{12,13}}, shuttle_p = 14, jog_p[2] = {15,16}, btn_p = 17;
+int tbar_old, fader_old[4], enc_old[4], shuttle_old, jog_old, btn_old[9];
+int tbar_midi = 1, fader_midi[4] = {2,3,4,5}, enc_midi[4] = {6,7,8,9}, shuttle_midi = 10, jog_midi = 11, btn_midi[9] = {12,13,14,15,16,17,18,19,20};
+int tbar_pin = 1, fader_pin[4] = {2,3,4,5}, enc_pin[4][2] = {{6,7},{8,9},{10,11},{12,13}}, shuttle_pin = 14, jog_pin[2] = {15,16}, btn_pin = 17;
 
-void midisend(channel, value){
+int midi_send(channel as int, value as int) {
   //TODO: Code to send Midi control channel changes
 }
 
-void midion(channel, value){
+int midi_on(note as int, value as int) {
   //TODO: code to send midi noteon
 }
 
-void midioff(channel){
+int midi_off(note as int) {
   //TODO: code to send midi noteoff
 }
 
-void midibtn(channel, value){
-  if value == 1 {
-    midion(channel, 127);
+int midi_button(note as int, value as int) {
+  if (value == 1) {
+    midi_on(note, 127);
   } else {
-    midioff(channel);
+    midi_off(note);
   }
 }
 
 void setup() {
   // put your setup code here, to run once:
-  //TODO: Analog read once directly to _o
-  fader_o = {0,0,0,0};
-  enc_o = {0,0,0,0};
+  //TODO: Analog read once directly to _old
+  fader_old = {0,0,0,0};
+  enc_old = {0,0,0,0};
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   //TODO: READ AND UPDATE VALUES
 
-  //COMPARE VALUES TO _O AND SEND MIDI UPDATES
+  //COMPARE VALUES TO _old AND SEND MIDI UPDATES
   for(i=0; i < 9, i++){
-    if btn[i] != btn_o[i] {
-      midibutton(btn_m[i], btn[i]);
+    if btn[i] != btn_old[i] {
+      midibutton(btn_midi[i], btn[i]);
     }
-    btn_o[i] = btn[i];
+    btn_old[i] = btn[i];
   }
   for(i=0; i < 4, i++){
-    if fader[i] != fader_o[i] {
-      midisend(fader_m[i], fader[i]/32);
+    if fader[i] != fader_old[i] {
+      midi_send(fader_midi[i], fader[i]/32);
     }
-    if enc[i] != enc_o[i] {
-      midisend(enc_m[i], (enc[i]-enc_o[i]+64));
+    if enc[i] != enc_old[i] {
+      midi_send(enc_midi[i], (enc[i]-enc_old[i]+64));
     }
-    fader_o[i] = fader[i];
-    enc_o[i] = enc[i];
+    fader_old[i] = fader[i];
+    enc_old[i] = enc[i];
   }
-  if tbar[i] != tbar_o[i] {
-    midisend(tbar_m[i], tbar[i]/32);
+  if tbar[i] != tbar_old[i] {
+    midi_send(tbar_midi[i], tbar[i]/32);
   }
-  if shuttle[i] != shuttle_o[i] {
-    midisend(shuttle_m[i], shuttle[i]);
+  if shuttle[i] != shuttle_old[i] {
+    midi_send(shuttle_midi[i], shuttle[i]);
   }
-  if jog[i] != jog_o[i] {
-    midisend(jog_m[i], (jog[i]-jog_o[i]+64));
+  if jog[i] != jog_old[i] {
+    midi_send(jog_midi[i], (jog[i]-jog_old[i]+64));
   }
 }
-
